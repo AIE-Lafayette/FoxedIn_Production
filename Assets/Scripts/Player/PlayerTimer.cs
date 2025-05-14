48df9fe9db37682d8b80b712c78ec76558f32989
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2025-05-14 16:55:53 -0500

    Worked more on the player timer.

--- Assets/Scripts/Player/PlayerTimer.cs
+++ Assets/Scripts/Player/PlayerTimer.cs
@@ -6,23 +6,11 @@ using UnityEngine;
 public class PlayerTimer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _timerText;
-    // Internal timer
-    private float _startTime = 0.0f;
-    // The time that has passed in whole seconds
+
+    // The time that has passed
     private float _currentTime;
-    private float _currentTimeMinutes;
 
     public float CurrentTime { get { return _currentTime; } }
-    public float CurrentTimeMinutes { get { return _currentTimeMinutes; } }
-
-    private void Start()
-    {
-        _currentTime = _startTime;
-        //if (_timerText)
-        //{
-        //    _timerText.text = _currentTime.ToString("0");
-        //}
-    }
 
     private void Update()
     {
@@ -31,25 +19,18 @@ public class PlayerTimer : MonoBehaviour
 
         //if (_timerText)
         //{
-        //    _timerText.text = _currentTimeMinutes.ToString("0");
-            
-        //}
-        
+        //    //_timerText.text = _currentTime.ToString("0");
 
-        //if (_currentTime >= 60.0f)
-        //{
-        //    _timerText.text = _currentTime.ToString("1: 00");
+
         //}
+        DisplayCurrentTime();
     }
-    //float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-    //float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 
-    //void AddMinute()
-    //{
-    //    if (_currentTime >= 60.0f)
-    //        {
-    //            _currentTimeMinutes += 1.0f;
-    //        }
-    //}
-
+    void DisplayCurrentTime()
+    {
+        //_currentTime += 1;
+        float minutes = Mathf.FloorToInt(_currentTime / 60);
+        float seconds = Mathf.FloorToInt(_currentTime % 60);
+        _timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
 }
