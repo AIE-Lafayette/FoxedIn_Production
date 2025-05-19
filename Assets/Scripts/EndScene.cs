2693dab1204f9baf6cec68aba104208a34d5a13a
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2025-05-19 14:52:31 -0500

    Added in controller rebinding asset and an end scene.

--- Assets/Scripts/EndScene.cs
+++ Assets/Scripts/EndScene.cs
@@ -1,27 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartScene : MonoBehaviour
+public class EndScene : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    public void DisplayCredits()
-    {
-
-    }
-
-    public void DisplayOptions()
-    {
-
     }
 }
